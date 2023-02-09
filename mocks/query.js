911f403f64c55403018d259e9f439162c3435c86
shinhwi <serendipity@shinhwiui-MacBookPro.local>
2023-02-09 14:59:21 +0900

    apply lint

--- mocks/query.js
+++ mocks/query.js
@@ -98,7 +98,7 @@ class Query {
         `FakeFirebaseError: Invalid query. Null only supports '==' and '!=' comparisons.`,
       );
     }
-    this._orderBy = this._orderBy.key ? this._orderBy : { key: key, direction: 'asc' };
+    this._orderBy = this._orderBy.key ? this._orderBy : { key, direction: 'asc' };
     this.filters.push({ key, comp, value });
     return result || this;
   }
